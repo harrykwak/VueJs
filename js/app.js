a85312d8d144f6d0a0cd35cb3d5e1d49906fb3a2
harry <harrykwak9595@gmail.com>
2021-05-12 22:51:25 +0900

    vue 공부 20210512

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-Vue.component("todo-footer", {
-  template: "<div>전역 컴포넌트</div>",
+Vue.component("child-component", {
+    props: ["propsdata"],
+    template: "<p>{{ propsdata }}</p>",
 });
 
-var local = {
-  template: "<div>지역 컴포넌트</div>",
-};
+Vue.component("sibling-component", {
+    props: ["propsdata"],
+    template: "<p>{{ propsdata }}</p>",
+});
 
 var app = new Vue({
-  el: "#app",
-  data: {
-    message: "분리한 내용 적용",
-  },
-  components: {
-    "todo-list": local,
-  },
+    el: "#app",
+    data: {
+        message: "Hello Vue!",
+        message2: "fucking test",
+    },
 });
